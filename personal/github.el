94242f8a92bff62031ed63ae614290d118918015
Callum White <callumw1991@gmail.com>
2016-02-02 14:21:40 +1100

    Add the super useful helm-open-github package

---
+++ personal/github.el
@@ -0,0 +1,7 @@
+(prelude-require-packages '(helm-open-github))
+
+;; set keys for helm-open-github
+(global-set-key (kbd "C-c o f") 'helm-open-github-from-file)
+(global-set-key (kbd "C-c o c") 'helm-open-github-from-commit)
+(global-set-key (kbd "C-c o i") 'helm-open-github-from-issues)
+(global-set-key (kbd "C-c o p") 'helm-open-github-from-pull-requests)
