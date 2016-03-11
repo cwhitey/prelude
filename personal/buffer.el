6086cbcc0181cfbf04617dd590fa250c0b62958a
Callum White <callumw1991@gmail.com>
2016-03-11 12:24:36 +1100

    More convenient keybindings for buffer cycling

--- personal/buffer.el
+++ personal/buffer.el
@@ -1,3 +1,7 @@
 (prelude-require-packages '(fasd))
 
 (global-fasd-mode 1)
+
+;; More convenient winnder-mode bindings
+(global-set-key (kbd "C-,") 'previous-buffer)
+(global-set-key (kbd "C-.") 'next-buffer)
