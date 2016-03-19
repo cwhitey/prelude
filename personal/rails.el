868f459e5951c6893b69ef9c6738211326b9c322
Callum White <callumw1991@gmail.com>
2016-03-19 16:47:20 +1100

    Eval-after-load for projectile-rails

--- personal/rails.el
+++ personal/rails.el
@@ -2,5 +2,6 @@
                            projectile-rails))
 
 ;; Turn on projectile-rails-mode when I'm in a rails app
-(add-hook 'projectile-mode-hook 'projectile-rails-on)
+(eval-after-load 'projectile-mode
+  (add-hook 'projectile-mode-hook 'projectile-rails-on))
 ;; TODO: Make sure robe-mode is working for all rails files (how to enable for slim files?)
