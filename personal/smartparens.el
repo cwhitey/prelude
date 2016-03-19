403550915d783428da00618d602fa027f78d2884
Callum White <callumw1991@gmail.com>
2016-03-20 01:18:47 +1100

    smartparens todo

--- personal/smartparens.el
+++ personal/smartparens.el
@@ -2,3 +2,7 @@
 (setq sp-autoskip-closing-pair t)
 ;; Skip closing pair even when backspace is pressed beforehand
 (setq sp-cancel-autoskip-on-backward-movement nil)
+
+;; TODO: start using sp-slurp-hybrid-sexp etc. for c-based languages
+;; If prog-mode and NOT lisp-mode?
+;; The hybrid smartparens functions work better on c-based languages
