3ddb26ea6c89059b7d8f66ce29873500f54126f8
Callum White <callumw1991@gmail.com>
2016-03-19 16:46:33 +1100

    Smartparens customizations

---
+++ personal/smartparens.el
@@ -0,0 +1,4 @@
+;; Always skip the closing pair
+(setq sp-autoskip-closing-pair t)
+;; Skip closing pair even when backspace is pressed beforehand
+(setq sp-cancel-autoskip-on-backward-movement nil)
