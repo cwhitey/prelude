7a088207b260533021680dcf13e726f72655cb3d
Callum White <callumw1991@gmail.com>
2016-03-21 10:50:25 +1100

    Set a default mono font in OSX

--- personal/editor.el
+++ personal/editor.el
@@ -30,3 +30,9 @@
 
 ;; I just want to use normal arrow keys thank you very much.
 (setq prelude-guru nil)
+
+;; Set default font (only verified in OSX)
+;; For other solutions see https://www.emacswiki.org/emacs/SetFonts
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :family "Menlo")
+  (set-face-attribute 'default nil :height 140))
