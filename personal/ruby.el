aeee3154afd69149f4b29354cd1c6dde331f4fec
Callum White <callumw1991@gmail.com>
2016-05-25 21:16:14 +1000

    Update default ruby version using chruby, and add ruby-refactor dependency

--- personal/ruby.el
+++ personal/ruby.el
@@ -1,8 +1,10 @@
-(prelude-require-packages '(robe chruby rspec-mode))
+(prelude-require-packages '(robe chruby rspec-mode ruby-refactor))
 
 ;; Make Emacs a Ruby IDE
 (add-hook 'ruby-mode-hook 'robe-mode)
+;; Ruby refactoring
+(add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
 ;; Set default ruby version within emacs
-(chruby "ruby-2.1.2")
+(chruby "ruby-2.2.3")
 ;; Don't use rake... just use the rspec gem
 (setq rspec-use-rake-when-possible nil)
