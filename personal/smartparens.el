b6b7574e448530b9cc0fccab616d2dd1614a573f
Callum White <callumw1991@gmail.com>
2016-05-25 21:19:16 +1000

    Notes about future support for slurping in c-like languages

--- personal/smartparens.el
+++ personal/smartparens.el
@@ -3,6 +3,6 @@
 ;; Skip closing pair even when backspace is pressed beforehand
 (setq sp-cancel-autoskip-on-backward-movement nil)
 
-;; TODO: start using sp-slurp-hybrid-sexp etc. for c-based languages
+;; TODO: start using sp-slurp-hybrid-sexp etc. for C-based languages
 ;; If prog-mode and NOT lisp-mode?
-;; The hybrid smartparens functions work better on c-based languages
+;; The hybrid smartparens functions work better on C-based languages
