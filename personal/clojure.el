ef88dcc874fa2a0b1216f86d22b548189e819565
Callum White <callumw1991@gmail.com>
2016-05-25 21:15:14 +1000

    Add user/reset shortcut for Cider repls

--- personal/clojure.el
+++ personal/clojure.el
@@ -7,3 +7,15 @@
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (add-hook 'clojure-mode-hook #'clj-refactor-hook)
+
+;; Shortcut for calling user/reset when using the reloaded workflow
+;; https://github.com/stuartsierra/reloaded
+(defun cider-repl-reset ()
+  (interactive)
+  (save-some-buffers)
+  (with-current-buffer (cider-current-repl-buffer)
+    (goto-char (point-max))
+    (insert "(user/reset)")
+    (cider-repl-return)))
+
+(global-set-key (kbd "C-c r") 'cider-repl-reset)
